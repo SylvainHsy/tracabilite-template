989448617f2ef0896de5ebac9dd9677fc36f9718
Shiyu Hu <100203369+SylvainHsy@users.noreply.github.com>
2022-09-13 16:39:18 +0200

    Update App.java

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -6,15 +6,27 @@ package com.iutblagnac.r303trace;
  */
 public class App 
 {
+/**
+ * 
+ * @param args la fonction pour retourner "hello world!"
+ */
     public static void main( String[] args )
     {
         System.out.println(hello());
     }
-
+/**
+ * @param string
+ * @return hello world!
+ */
     public static String hello() {
-        return  "Hello World!" ;
+        return  "Hello World!";
     }
 
+/**
+ * 
+ * @param param
+ * @return param
+ */
     public static String hello(String param) {
         return  param ;
     }
